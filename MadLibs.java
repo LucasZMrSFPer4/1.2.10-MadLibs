102071883e8804b2bdfb68e4fc7dac7f19f3c0cf
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2024-10-07 20:40:25 -0700

    git commit

--- MadLibs.java
+++ MadLibs.java
@@ -4,52 +4,40 @@ public class MadLibs
 {
   public static void main(String[] args)
   {
-    String incompleteMadLib1 = new String("I went to the animal <noun>, the <plural_noun> and the <number> beasts were there."); // Mad Lib 1
-    String incompleteMadLib2 = new String("Once upon a time in a <adjective> land, a <noun> <adverb> grew."); // Mad Lib 2
-    String incompleteMadLib3 = new String("I am not <adjective>, <conjunction> I'm not going to <verb> yet."); // Mad Lib 3
+    String incompleteMadLib = new String("I went to the animal <noun>, the <plural noun> and the <number> beasts were there.");
+    /* String incompleteMadLib = new String("Once upon a time in a <adjective> land, a <noun> <adverb> grew.");
+    String incompleteMadLib = new String("I am not <adjective>, <conjunction> I'm not going to <verb> yet."); */
 
     Scanner sc = new Scanner(System.in);
-    System.out.print("Prompts:\n1. Going to Disneyland\n2. Fairytale\n3. Time to Eat!\n"); // the user chooses a prompt
-    String promptChoice = new String(sc.nextLine());
 
-    if (promptChoice.equals("1")) { // If the user chooses prompt 1, he/she will choose a noun, a plural noun, and a number.
-      System.out.print("Noun: ");
-      String noun = new String(sc.nextLine());
-      System.out.print("Plural Noun: ");
-      String pluralNoun = new String(sc.nextLine());
-      System.out.print("Number: ");
-      String number = new String(sc.nextLine());
-      String substring1 = incompleteMadLib1.substring(0, 21);
-      String substring2 = incompleteMadLib1.substring(27, 33);
-      String substring3 = incompleteMadLib1.substring(46, 55);
-      String substring4 = incompleteMadLib1.substring(63, incompleteMadLib1.length());
-      System.out.print(substring1 + noun + substring2 + pluralNoun + substring3 + number + substring4);
-    }
+    String madLib = new String(incompleteMadLib);
 
-    if (promptChoice.equals("2")) { // If the user chooses prompt 2, he/she will choose an adjective, a noun, and an adverb.
-      System.out.print("Adjective: ");
-      String adjective = new String(sc.nextLine());
-      System.out.print("Noun: ");
-      String noun = new String(sc.nextLine());
-      System.out.print("Adverb: ");
-      String adverb = new String(sc.nextLine());
-      String substring1 = incompleteMadLib2.substring(0, 22);
-      String substring2 = incompleteMadLib2.substring(33, 42);
-      String substring3 = incompleteMadLib2.substring(57, incompleteMadLib2.length());
-      System.out.print(substring1 + adjective + substring2 + noun + " " + adverb + substring3);
-    }
+    int openBracket = madLib.indexOf("<");
+    int closeBracket = madLib.indexOf(">");
+    String restOfMadLib1 = madLib.substring(closeBracket + 1, madLib.length());
+    String partOfSpeech1 = madLib.substring(openBracket + 1, closeBracket);
+    System.out.print("Enter a " + partOfSpeech1 + ": ");
+    String missingWord1 = new String(sc.nextLine());
+    String substring1 = new String(madLib.substring(0, openBracket) + missingWord1);
 
-    if (promptChoice.equals("3")) { // If the user chooses prompt 3, he/she will choose an adjective, a conjunction, and a verb.
-      System.out.print("Adjective: ");
-      String adjective = new String(sc.nextLine());
-      System.out.print("Conjunction: ");
-      String conjunction = new String(sc.nextLine());
-      System.out.print("Verb: ");
-      String verb = new String(sc.nextLine());
-      String substring1 = incompleteMadLib3.substring(0, 9);
-      String substring2 = incompleteMadLib3.substring(35, 53);
-      String substring3 = incompleteMadLib3.substring(59, incompleteMadLib3.length());
-      System.out.print(substring1 + adjective + ", " + conjunction + substring2 + verb + substring3);
-    }
+    openBracket = restOfMadLib1.indexOf("<");
+    closeBracket = restOfMadLib1.indexOf(">");
+    String restOfMadLib2 = restOfMadLib1.substring(closeBracket + 1, restOfMadLib1.length());
+    String partOfSpeech2 = restOfMadLib1.substring(openBracket + 1, closeBracket);
+    System.out.print("Enter a " + partOfSpeech2 + ": ");
+    String missingWord2 = new String(sc.nextLine());
+    String substring2 = new String(restOfMadLib1.substring(0, openBracket) + missingWord2);
+
+    openBracket = restOfMadLib2.indexOf("<");
+    closeBracket = restOfMadLib2.indexOf(">");
+    String restOfMadLib3 = restOfMadLib2.substring(closeBracket + 1, restOfMadLib2.length());
+    String partOfSpeech3 = restOfMadLib2.substring(openBracket + 1, closeBracket);
+    System.out.print("Enter a " + partOfSpeech3 + ": ");
+    String missingWord3 = new String(sc.nextLine());
+    String substring3 = new String(restOfMadLib2.substring(0, openBracket) + missingWord3);
+
+    String substring4 = new String(restOfMadLib3.substring(0, restOfMadLib3.length()));
+
+    System.out.print(substring1 + substring2 + substring3 + substring4);
   }
 }
